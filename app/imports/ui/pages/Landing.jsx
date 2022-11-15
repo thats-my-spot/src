cf99996c6dd5bb4f2f3bf98ed324828c052931ed
yertsti <77418067+yertsti@users.noreply.github.com>
2022-11-15 09:52:39 -1000

    finished landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,21 +1,50 @@
 import React from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
-  <Container id="landing-page" fluid className="py-3">
-    <Row className="align-middle text-center">
-      <Col xs={4}>
-        <Image roundedCircle src="/images/meteor-logo.png" width="150px" />
-      </Col>
+  <Container fluid className=" p-0 m-0 w-100">
+    <div className="background-image py-3 w-100 p-0 m-0 text-center align-text-bottom">
+      <a href="#instruction" className="h1"><button type="button" className="btn btn-light rounded-pill">Instructions</button></a>
+    </div>
+    <div id="instruction" className="background-image2">
+      <Row>
+        <Col className="d-flex flex-column justify-content-center text-center text-white h1">
+          HOW TO PARK
+        </Col>
+        <Row className="d-flex justify-content-center">
+          <Col className="text-center text-white">
+            <h1>How to get a parking stall with an account</h1>
+            <Container>Instructions</Container>
 
-      <Col xs={8} className="d-flex flex-column justify-content-center">
-        <h1>Welcome to this template</h1>
-        <p>Now get to work and modify this app!</p>
-      </Col>
-
-    </Row>
+            <Container>Already have an account? Sign in below</Container>
+            <a href="/signin">
+              <button type="button" className="btn btn-light rounded-pill">Sign in</button>
+            </a>
+            <Container>
+              Don&#39;t have an account? Sign up below
+            </Container>
+            <a href="/signup">
+              <button type="button" className="btn btn-light rounded-pill">Sign up</button>
+            </a>
+          </Col>
+          <Col className="text-center text-white">
+            <h1>How to get a parking stall without an account</h1>
+            <Container>Instructions</Container>
+            <a href="#">
+              <button type="button" className="btn btn-light rounded-pill">Look for a stall</button>
+            </a>
+          </Col>
+        </Row>
+      </Row>
+      <Row>
+        <Container className="text-white d-flex justify-content-center">
+          <a href="#root" className="h1"><button type="button" className="btn btn-light rounded-pill">Back to top</button></a>
+        </Container>
+      </Row>
+    </div>
   </Container>
+
 );
 
 export default Landing;
