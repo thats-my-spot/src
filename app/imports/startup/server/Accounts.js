7d2224badfb0e506298fc42cc0f2d9bfa31e069e
yertsti <77418067+yertsti@users.noreply.github.com>
2022-11-16 22:44:30 -1000

    Forgot comma

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -22,7 +22,7 @@ const createUser = (email, password, hasPass, role) => {
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.forEach(({ email, password, hasPass, role }) => createUser(email, password, hasPass, role,));
+    Meteor.settings.defaultAccounts.forEach(({ email, password, hasPass, role }) => createUser(email, password, hasPass, role));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
