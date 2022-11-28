253f60ba7c2eda12851668ee4678b4326431d598
Jayden Tactay <75406323+jaydent22@users.noreply.github.com>
2022-11-27 20:07:25 -1000

    Edited availability page

--- app/imports/ui/pages/Availability.jsx
+++ app/imports/ui/pages/Availability.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col, Container, Row, Table } from 'react-bootstrap';
+import { Button, Col, Container, Row, Table } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import { Stalls } from '../../api/stalls/Stalls';
-import StallItem from '../components/StallItem';
 import LoadingSpinner from '../components/LoadingSpinner';
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
@@ -13,7 +12,7 @@ const Availability = () => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Stalls.userPublicationName);
+    const subscription = Meteor.subscribe(Stalls.adminPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Stuff documents
@@ -38,17 +37,32 @@ const Availability = () => {
                 <th> 3</th>
                 <th> 4</th>
                 <th> 5</th>
+                <th> 6</th>
+                <th> 7</th>
+                <th> 8</th>
+                <th> 9</th>
+                <th> 10</th>
+                <th> 11</th>
+                <th> 12</th>
+                <th> 13</th>
+                <th> 14</th>
+                <th> 15</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <th height={200}> </th>
-                <th height={200}> </th>
+                {stalls.map((stall) => <td height={200} style={{ backgroundColor: (stall.owner === 'empty' ? 'lightgreen' : 'red') }}> </td>)}
               </tr>
-              {stalls.map((stall) => <StallItem key={stall.stallId} stall={stall} />)}
             </tbody>
-
           </Table>
+          <Row className="justify-content-center">
+            <Col className="col-4">
+              <Button>Previous level</Button>
+            </Col>
+            <Col className="col-4">
+              <Button>Next level</Button>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
