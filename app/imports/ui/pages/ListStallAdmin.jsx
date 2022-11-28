d98f98e3a03a498aee2661723d127e4ae6656aa2
Jayden Tactay <75406323+jaydent22@users.noreply.github.com>
2022-11-27 19:33:18 -1000

    Edited availability to display available stalls

--- app/imports/ui/pages/ListStallAdmin.jsx
+++ app/imports/ui/pages/ListStallAdmin.jsx
@@ -36,7 +36,9 @@ const ListStallAdmin = () => {
               </tr>
             </thead>
             <tbody>
-              {stalls.map((stall) => <StallItemAdmin key={stall.stallId} stall={stall} />)}
+              {stalls.map(function (stall) {
+                return (stall.owner === 'empty') ? '' : <StallItemAdmin key={stall.stallId} stall={stall} />;
+              })}
             </tbody>
           </Table>
         </Col>
