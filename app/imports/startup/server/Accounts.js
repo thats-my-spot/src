fda4a8be8c4225bce2ae3574422b5a8b8898a8c0
yertsti <77418067+yertsti@users.noreply.github.com>
2022-11-29 11:53:29 -1000

    Revert "updated the default data to take in license plate"

--- app/imports/startup/server/Accounts.js
+++ app/imports/startup/server/Accounts.js
@@ -4,16 +4,13 @@ import { Roles } from 'meteor/alanning:roles';
 
 /* eslint-disable no-console */
 
-const createUser = (email, password, hasPass, licensePlate, role) => {
+const createUser = (email, password, hasPass, role) => {
   console.log(`  Creating user ${email}.`);
   const userID = Accounts.createUser({
     username: email,
     email: email,
     password: password,
-    profile: {
-      hasPass: hasPass,
-      licensePlate: licensePlate,
-    }
+    hasPass: hasPass,
   });
   if (role === 'admin') {
     Roles.createRole(role, { unlessExists: true });
@@ -25,7 +22,7 @@ const createUser = (email, password, hasPass, licensePlate, role) => {
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
     console.log('Creating the default user(s)');
-    Meteor.settings.defaultAccounts.forEach(({ email, password, hasPass, licensePlate, role }) => createUser(email, password, hasPass, licensePlate, role));
+    Meteor.settings.defaultAccounts.forEach(({ email, password, hasPass, role }) => createUser(email, password, hasPass, role));
   } else {
     console.log('Cannot initialize the database!  Please invoke meteor with a settings file.');
   }
