2b9b32dee384c85a2b81974ded0359fa9de5fe11
Eduardo Kho Jr <eduardok@hawaii.edu>
2022-11-29 16:41:46 -1000

    pretty landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -12,23 +12,36 @@ const Landing = () => (
         <Col className="d-flex flex-column justify-content-center text-center text-white h1">
           HOW TO PARK
         </Col>
-        <Row className="d-flex justify-content-center">
-          <Col className="text-center text-white">
-            <div id="text-bg"><h1>Have a UH account?</h1>
-              <AccInstr />
-            </div>
-          </Col>
-          <Col className="text-center text-white">
-            <div id="text-bg"><h1>Guest?</h1>
-              <div>
-                <img id="medium-image" src="https://manoa.hawaii.edu/commuter/wp-content/uploads/sites/6/2022/11/students.jpg" alt="nothing" />
+        <Row>
+          <div className="d-flex justify-content-center">
+            <Col className="text-center text-white">
+              <div id="text-bg"><h1>Have a UH account?</h1>
+                <div>
+                  <img id="medium-image" src="https://manoa.hawaii.edu/commuter/wp-content/uploads/sites/6/2022/11/students.jpg" alt="nothing" />
+                </div>
+                <div id="small-padd">
+                  <AccInstr />
+                </div>
               </div>
-              <div id="small-padd">
-                <NoAccInstr />
+            </Col>
+            <Col className="text-center text-white">
+              <div id="text-bg"><h1>Guest?</h1>
+                <div>
+                  <img id="medium-image" src="https://manoa.hawaii.edu/commuter/wp-content/uploads/sites/6/2022/11/special-events.jpg" alt="nothing" />
+                </div>
+                <div id="small-padd">
+                  <NoAccInstr />
+                </div>
               </div>
-            </div>
-          </Col>
+            </Col>
+          </div>
         </Row>
+      </Row>
+      <Row id="small-padd">
+        <Col className="text-white h1" md={{ span: 4, offset: 1 }}>
+          Frequently Asked Questions
+        </Col>
+        <hr />
       </Row>
       <Row>
         <Container className="text-white d-flex justify-content-center">
@@ -48,21 +61,24 @@ const AccInstr = () => {
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
+      <Button variant="success" onClick={handleShow}>
         Instructions
       </Button>
 
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose} className="modal-lg">
         <Modal.Header closeButton>
           <Modal.Title>Instructions</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <ol>1.</ol>
-          <ol>2.</ol>
-          <ol>3.</ol>
+          <div>1. Sign up for a <i>That's My Spot Account</i> with your UH Manoa account <a href="/signup">here.</a></div>
+          <div>2. Visit the <a href="/availability">availability</a> page to see open stalls.</div>
+          <div>3. <a href="/reserve">Reserve</a> and park!</div>
+          <div id="small-padd">
+            Already have an account?
+            <a href="/reserve">Sign in</a>
+          </div>
         </Modal.Body>
-        <Modal.Footer>
-        </Modal.Footer>
+        <Modal.Footer />
       </Modal>
     </>
   );
@@ -76,7 +92,7 @@ const NoAccInstr = () => {
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
+      <Button variant="success" onClick={handleShow}>
         Instructions
       </Button>
       <Modal show={show} onHide={handleClose}>
@@ -84,10 +100,9 @@ const NoAccInstr = () => {
           <Modal.Title>Instructions</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div>Already have an account? Sign in below</div>
-          <a href="/reserve">
-            <button type="button" className="btn btn-light rounded-pill">Sign in</button>
-          </a>
+          <div>1. Visit the <a href="/reserve">Reserve</a> page </div>
+          <div>2. Visit the <a href="/availability">availability</a> page to see open stalls.</div>
+          <div>3. <a href="/reserve">Reserve</a> and park!</div>
         </Modal.Body>
       </Modal>
     </>
