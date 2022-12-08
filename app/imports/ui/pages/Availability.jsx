395134bc21d838ae780214c8a3434571f2087da1
Jayden Tactay <75406323+jaydent22@users.noreply.github.com>
2022-12-08 09:26:46 -1000

    Render availability for logged in users, other minor tweaks

--- app/imports/ui/pages/Availability.jsx
+++ app/imports/ui/pages/Availability.jsx
@@ -23,7 +23,7 @@ const Availability = () => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Stalls.availablePublicationName);
+    const subscription = Meteor.subscribe(Stalls.adminPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Stuff documents
