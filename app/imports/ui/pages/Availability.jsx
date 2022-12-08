af6b5671cef7216a413d894dbf76bcf93e4ce2b5
yertsti <77418067+yertsti@users.noreply.github.com>
2022-12-08 09:56:21 -1000

    Can reserve stall by clicking on green square

--- app/imports/ui/pages/Availability.jsx
+++ app/imports/ui/pages/Availability.jsx
@@ -3,8 +3,10 @@ import { Meteor } from 'meteor/meteor';
 import { Col, Container, Row, Table } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
 import Pagination from 'react-bootstrap/Pagination';
-import { Stalls } from '../../api/stalls/Stalls';
 import LoadingSpinner from '../components/LoadingSpinner';
+import { Link } from 'react-router-dom';
+import swal from 'sweetalert';
+import { Stalls } from '../../api/stalls/Stalls';
 
 const active = 1;
 const items = [];
@@ -15,6 +17,18 @@ for (let i = 1; i <= 3; i++) {
     </Pagination.Item>,
   );
 }
+const submit = (user, _id) => {
+  // const stall = Stalls.collection.findOne(_id);
+  Stalls.collection.update(_id, { $set: { owner: user.username, licensePlate: user.profile.licensePlate } }, (error) => {
+    if (error) {
+      swal('Error', error.message, 'error');
+    } else {
+      swal('Success', 'Profile updated successfully', 'success');
+      //setRedirectToRef(true);
+    }
+  });
+};
+
 
 /* Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 const Availability = () => {
@@ -28,6 +42,8 @@ const Availability = () => {
     const rdy = subscription.ready();
     // Get the Stuff documents
     const stallItems = Stalls.collection.find({}).fetch();
+    const user = Meteor.users.findOne(Meteor.userId());
+    //console.log(Stalls.collection.find({ owner: { $in: 'admin@foo.com' } }));
     return {
       stalls: stallItems,
       ready: rdy,
@@ -42,7 +58,24 @@ const Availability = () => {
           </Col>
           <Table striped bordered hover>
             <thead>
+              <tr className="text-center">
+                <th colSpan={5}>Floor 1</th>
+                <th colSpan={5}>Floor 2</th>
+                <th colSpan={5}>Floor 3</th>
+              </tr>
+            </thead>
+            <thead>
               <tr>
+                <th> 1</th>
+                <th> 2</th>
+                <th> 3</th>
+                <th> 4</th>
+                <th> 5</th>
+                <th> 1</th>
+                <th> 2</th>
+                <th> 3</th>
+                <th> 4</th>
+                <th> 5</th>
                 <th> 1</th>
                 <th> 2</th>
                 <th> 3</th>
@@ -54,7 +87,11 @@ const Availability = () => {
               <tr className="p-0 m-0">
                 {stalls.map(function (stall) {
                   return (stall.owner === 'empty') ? (
-                    <td height={200} width={100} style={{ background: 'lightgreen' }} className="bg-light-green m-0 p-0"><a href="/payment" style={{ display: 'block', position: 'relative', height: '100%', width: '100%' }}>‎</a></td>
+                    <td height={200} width={100} style={{ background: 'lightgreen' }} className="bg-light-green m-0 p-0">
+                      <Link onClick={() => submit(Meteor.users.findOne(Meteor.userId()), stall._id)} to="/stall" style={{ display: 'block', position: 'relative', height: '100%', width: '100%' }}>
+                        ‎
+                      </Link>
+                    </td>
                   )
                     : <td height={200} width={100} style={{ backgroundColor: 'red' }}> </td>;
                 })}
